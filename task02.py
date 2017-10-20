1b9ab31239f8e066dac1086933dce54dee088d7d
= <sp403250@dal.ca>
2017-10-20 15:08:17 -0300

    halifaxweather

--- task02.py
+++ task02.py
@@ -6,5 +6,38 @@ def weathers(halifax):
     condition = location.condition()
     p = condition['text']
     print(p)
+    t = []
+    d = []
+    h = []
+    l = []
+    c = []
+    j = []
+    y = []
+    
+    for forecasts in location.forecast():
+        t.append(forecasts['text']
+        d.append(forecasts['date']
+        h.append(forecasts['high']
+        l.append(forecasts['low']
+
+    for item in l:
+        temp = int(item)
+        c.append(temp)
+    print max("Max temp in 5 days is:" c)
+    
+    for item in h:
+        temp = int(item)
+        j.append(temp)
+    print max("Max temp in 5 days is :" j)
+    
+    for item in t:
+        if item = "rainy"
+
+weathers(halifax)
+
+
+
+
+
 
 
