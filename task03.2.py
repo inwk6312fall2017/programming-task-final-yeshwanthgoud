0b5630de2b1af2c9a80e0f45ae0b70382bfafe58
= <sp403250@dal.ca>
2017-10-20 15:55:15 -0300

    accesslistnames

--- task03.2.py
+++ task03.2.py
@@ -9,3 +9,9 @@ for line in  my_file:
         if word == "access-list":
             list_of_access.append(words)
 
+for index in range(len(list_of_access)):
+    my_dict[tuple(list_of_access[index][2:])] = list_of_access[index]
+
+print (my_dict)
+
+
