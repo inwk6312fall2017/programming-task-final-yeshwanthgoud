975befaa6c42b208f7cdaa651c4b566882a9d13c
= <sp403250@dal.ca>
2017-10-20 16:10:32 -0300

    biggestword

--- task01.py
+++ task01.py
@@ -1,7 +1,13 @@
+my_f1 = ("Book1.txt")
+my_f2 = ("Book2.txt")
+my_f3 = ("Book3.txt")
+
 def Words():
-    qfile=open('Book1.txt','Book2.txt','Book3.txt','r')
+    qfile=open('my_f1','my_f2','my_f3','r')
     biggest=''
     for line in qfile:
         if len(line)>len(biggest):
             biggest=line
         return biggest
+print (words)
+
